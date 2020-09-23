f2226bef90eead307471289da28957ec59777931
Gary Shaw <jrxiao98@gmail.com>
2020-09-23 19:03:20 +0800

    LeetCode #1528

---
+++ solutions/1528.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String restoreString(String s, int[] indices) {
+        char[] str = new char[indices.length];
+        for (int i = 0; i < indices.length; i++) {
+            str[indices[i]] = s.charAt(i);
+        }
+        return String.valueOf(str);
+    }
+}

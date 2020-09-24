0d22b4513670af0de71c11c31d513bdffa133eed
Gary Shaw <jrxiao98@gmail.com>
2020-09-24 21:02:56 +0800

    LeetCode #709

---
+++ solutions/709.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String toLowerCase(String str) {
+        char[] s = str.toCharArray();
+        for (int i = 0; i < s.length; i++) {
+            if (s[i] >= 'A' && s[i] < 'Z') {
+                s[i] += 32;
+            }
+        }
+        return String.valueOf(s);
+    }
+}

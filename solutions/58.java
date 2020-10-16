739844c7abf716ea7ebabed7a5f709c2d08191ea
Gary Shaw <jrxiao98@gmail.com>
2020-10-16 22:52:29 +0800

    LeetCode #58

---
+++ solutions/58.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int l = s.length();
+        int cnt = 0;
+        for (int i = l - 1; i >= 0; i--) {
+            if (s.charAt(i) != ' ') {
+                cnt++;
+            } else if (cnt != 0) {
+                return cnt;
+            }
+        }
+        return cnt;
+    }
+}

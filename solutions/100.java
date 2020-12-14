0bb2e7b461b77abb6da13dda9e42a284c2f19278
Gary Shaw <jrxiao98@gmail.com>
2020-12-14 19:57:28 +0800

    LeetCode #100

---
+++ solutions/100.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (null == p && null == q) {
+            return true;
+        } else if (p == null || q == null) {
+            return false;
+        }
+
+        if (p.val != q.val) {
+            return false;
+        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+
+    }
+}
